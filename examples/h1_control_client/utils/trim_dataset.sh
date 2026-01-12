aaae65b7aeb3befc2cb1db1180dfcb1884e020a1
HariSrikanth <harinsrikanth@gmail.com>
2026-01-12 14:59:58 -0800

    before action chunk advantage cut

--- examples/h1_control_client/utils/trim_dataset.sh
+++ examples/h1_control_client/utils/trim_dataset.sh
@@ -1,5 +1,5 @@
 python trim_dataset.py \
---input_path ../h1_data_raw/insert_bottle_jan9/episode_3.hdf5 \
---output_path ../h1_data_processed/insert_bottle_jan9/episode_20.hdf5 \
---start_frame 1520 \
---end_frame 1860
+--input_path ../h1_data_processed/fold_towel_jan9/episode_20.hdf5 \
+--output_path ../h1_data_processed/fold_towel_jan10/episode_20.hdf5 \
+--start_frame 0 \
+--end_frame 590
